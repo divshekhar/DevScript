2f11af004ddec814f5536356c4af70b09093a404
Divyanshu Shekhar <imdshekhar@gmail.com>
2022-12-18 15:10:47 +0530

    (feat): parse grouped expressions

--- parser/operator_precedence_test.go
+++ parser/operator_precedence_test.go
@@ -76,6 +76,27 @@ func TestOperatorPrecedenceParsing(t *testing.T) {
 			"3 < 5 == true",
 			"((3 < 5) == true)",
 		},
+		// After adding grouped expressions
+		{
+			"1 + (2 + 3) + 4",
+			"((1 + (2 + 3)) + 4)",
+		},
+		{
+			"(5 + 5) * 2",
+			"((5 + 5) * 2)",
+		},
+		{
+			"2 / (5 + 5)",
+			"(2 / (5 + 5))",
+		},
+		{
+			"-(5 + 5)",
+			"(-(5 + 5))",
+		},
+		{
+			"!(true == true)",
+			"(!(true == true))",
+		},
 	}
 
 	for _, tt := range tests {
--- parser/parser.go
+++ parser/parser.go
@@ -70,6 +70,7 @@ func New(lex *lexer.Lexer) *Parser {
 	parser.registerPrefix(token.MINUS, parser.parsePrefixExpression)
 	parser.registerPrefix(token.TRUE, parser.parseBoolean)
 	parser.registerPrefix(token.FALSE, parser.parseBoolean)
+	parser.registerPrefix(token.LPAREN, parser.parseGroupedExpression)
 
 	// Initialize the infixParseFns map
 	parser.infixParseFns = make(map[token.TokenType]infixParseFn)
