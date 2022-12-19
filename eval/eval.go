fc143ffbdad0ac0d1d0c14823729331905548379
Divyanshu Shekhar <imdshekhar@gmail.com>
2022-12-19 09:10:04 +0530

    (feat): one reference to null and boolean

--- eval/eval.go
+++ eval/eval.go
@@ -5,6 +5,12 @@ import (
 	"devscript/object"
 )
 
+var (
+	NULL  = &object.Null{}
+	TRUE  = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+)
+
 // Eval evaluates an AST
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
@@ -15,10 +21,18 @@ func Eval(node ast.Node) object.Object {
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
 	case *ast.Boolean:
-		return &object.Boolean{Value: node.Value}
+		return nativeBoolToBooleanObject(node.Value)
 	}
 
-	return nil
+	return NULL
+}
+
+// nativeBoolToBooleanObject converts a native bool to a Boolean object
+func nativeBoolToBooleanObject(input bool) *object.Boolean {
+	if input {
+		return TRUE
+	}
+	return FALSE
 }
 
 // evalStatements evaluates a list of statements
