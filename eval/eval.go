693080a6e8e23d84e43fddadf600f2d99fff8cc8
Divyanshu Shekhar <imdshekhar@gmail.com>
2022-12-19 09:37:38 +0530

    (feta): prefix operator eval

--- eval/eval.go
+++ eval/eval.go
@@ -9,6 +9,7 @@ var (
 	NULL  = &object.Null{}
 	TRUE  = &object.Boolean{Value: true}
 	FALSE = &object.Boolean{Value: false}
+	ZERO  = &object.Integer{Value: 0}
 )
 
 // Eval evaluates an AST
@@ -19,12 +20,57 @@ func Eval(node ast.Node) object.Object {
 	case *ast.ExpressionStatement:
 		return Eval(node.Expression)
 	case *ast.IntegerLiteral:
+		// Zero is a special case
+		if node.Value == 0 {
+			return ZERO
+		}
 		return &object.Integer{Value: node.Value}
 	case *ast.Boolean:
 		return nativeBoolToBooleanObject(node.Value)
+	case *ast.PrefixExpression:
+		right := Eval(node.Right)
+		return evalPrefixExpression(node.Operator, right)
 	}
 
 	return NULL
+}
+
+// evalPrefixExpression evaluates a prefix expression
+func evalPrefixExpression(operator string, right object.Object) object.Object {
+	switch operator {
+	case "!":
+		return evalBangOperatorExpression(right)
+	case "-":
+		return evalMinusPrefixOperatorExpression(right)
+	default:
+		return NULL
+	}
+}
+
+// evaluates a bang operator expression
+func evalBangOperatorExpression(right object.Object) object.Object {
+	switch right {
+	case TRUE:
+		return FALSE
+	case FALSE:
+		return TRUE
+	case NULL:
+		return TRUE
+	case ZERO:
+		return TRUE
+	default:
+		return FALSE
+	}
+}
+
+// evaluates a minus prefix operator expression
+func evalMinusPrefixOperatorExpression(right object.Object) object.Object {
+	if right.Type() != object.INTEGER_OBJ {
+		return NULL
+	}
+
+	value := right.(*object.Integer).Value
+	return &object.Integer{Value: -value}
 }
 
 // nativeBoolToBooleanObject converts a native bool to a Boolean object
--- eval/eval_test.go
+++ eval/eval_test.go
@@ -74,3 +74,40 @@ func testBooleanObject(t *testing.T, evaluated object.Object, expected bool) boo
 
 	return true
 }
+
+func TestBangOperator(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected bool
+	}{
+		{"!true", false},
+		{"!false", true},
+		{"!5", false},
+		{"!!true", true},
+		{"!!false", false},
+		{"!!5", true},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testBooleanObject(t, evaluated, tt.expected)
+	}
+}
+
+func TestMinusPrefixOperator(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int64
+	}{
+		{"-5", -5},
+		{"-10", -10},
+		{"--5", 5},
+		{"-(-10)", 10},
+		{"-(10)", -10},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testIntegerObject(t, evaluated, tt.expected)
+	}
+}
